7b67dada0aae14f52f89f2696bba3070345c7122
brent lab user <chasem@n134.htcf>
2019-12-20 18:16:40 -0600

    fixed typos in 1.0.lua

--- 1.0.lua
+++ 1.0.lua
@@ -1,8 +1,15 @@
---help
+help(
+[[
+This module loads the Brent lab rnaseq_pipeline.
+]])
+
+local pkgName = myModuleFullName()
+local fullVersion = myModuleVersion ()
+local base = "/opt/apps/labs/mblab/software/rnaseq_pipeline/1.0"
 
 -- display this info with module whatis
-whatis("Name: " ...pkgName)
-whatis("Version: "..fullVersion)
+whatis("Name: " ..pkgName)
+whatis("Version: " ..fullVersion)
 whatis("Description: The Brent lab RNA-Seq pipeline")
 whatis("Location: " ..base)
 whatis("URL: https://gitlab.com/brentlab/rnaseq_pipe")
@@ -17,12 +24,13 @@ depends_on("py-openpyxl/2.4.5-python-3.6.5")
 
 -- message to print on module load
 if(mode() == 'load') then
-   LmodMessage("nBrent lab rnaseq_pipeline is loaded!\n\tPleasesee https://gitlab.com/brentlab/rnaseq_pipe/-/wikis/home for usage instructions.\n")
+   LmodMessage("nBrent lab rnaseq_pipeline is loaded!\n\tPlease see https://gitlab.com/brentlab/rnaseq_pipe/-/wikis/home for usage instructions.\n")
 end
 
 -- message to print on module unload
 if (mode() == "unload") then
    LmodMessage("\nmodule rnaseq_pipeline and all dependencies automatically loaded by rnaseq_pipeline are unloaded.\n")
+end
 
 -- set environmental variables for R and python dependencies
 setenv("R_LIBS_USER", "/opt/apps/labs/mblab/software/rnaseq_pipeline/v1.0_r_dependencies")
@@ -31,7 +39,7 @@ setenv("PYTHONPATH", "/opt/apps/labs/mblab/software/rnaseq_pipeline/v1.0_python_
 
 -- prepend the following to $PATH
 prepend_path("PATH", base)
-prepent_path("PATH", pathJoin(base, "tools"))
+prepend_path("PATH", pathJoin(base, "tools"))
 
 
 
