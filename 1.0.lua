d2ce0e8f799bba75dae6945f8dff846041c65763
brent lab user <chasem@htcf.htcf>
2020-01-07 13:39:03 -0600

    now wrking with ml rnaseq_pipeline in rnaseq_pipeline env copied from /opt/...../modules/miniconda/envs to user .conda/envs

--- 1.0.lua
+++ 1.0.lua
@@ -15,36 +15,29 @@ whatis("Location: " ..base)
 whatis("URL: https://gitlab.com/brentlab/rnaseq_pipe")
 
 -- load main tools (see system requirements on the gitlab wiki)
-depends_on("r/3.5.3-python-3.6.5")
+depends_on("miniconda")
 depends_on("igv/2.4.7")
-depends_on("py-pandas/0.24.1-python-3.6.5")
-depends_on(" py-numpy/1.16.2-python-3.6.5")
-depends_on("py-pyyaml/3.13-python-3.6.5")
-depends_on("py-openpyxl/2.4.5-python-3.6.5")
 
 -- message to print on module load
 if(mode() == 'load') then
    LmodMessage("\nBrent lab rnaseq_pipeline is loaded!\n\tPlease see https://gitlab.com/brentlab/rnaseq_pipe/-/wikis/home for usage instructions.\n")
 end
 
--- message to print on module unload
-if (mode() == "unload") then
-   LmodMessage("\nmodule rnaseq_pipeline and all dependencies automatically loaded by rnaseq_pipeline are unloaded.\n")
-end
-
--- set environmental variables for R and python dependencies
-setenv("R_LIBS_USER", "/opt/apps/labs/mblab/software/rnaseq_pipeline/v1.0_r_dependencies")
-setenv("R_INCLUDE_DIR", "/opt/apps/labs/mblab/software/rnaseq_pipeline/v1.0_r_dependencies")
---setenv("PYTHONPATH", "/opt/apps/labs/mblab/software/rnaseq_pipeline/v1.0_python_dependencies")
-
 -- prepend the following to $PATH
 prepend_path("PATH", base)
 prepend_path("PATH", pathJoin(base, "tools"))
 
+-- see test_conda_env in the rnaseq_pipeline code repo. This simply tests whether the directory $HOME/.conda/envs exists. If it does not, the rnaseq_pipeline_env directory is cloned by conda
+-- into $HOME/.conda/envs.
+-- see the miniconda module help for more details
+if (not isDir("$HOME/.conda/envs/rnaseq_pipeline")) then
+  execute{cmd="test_conda_env",modeA={"load"}}
+end
 
+-- load/unload virtual environment located in $HOME/.conda/envs/rnaseq_pipeline
+execute{cmd="conda activate rnaseq_pipeline",modeA={"load"}}
 
-
-
-
-
-
+-- message to print on module unload
+if (mode() == "unload") then
+   LmodMessage("\nmodule rnaseq_pipeline and all dependencies and environments loaded by rnaseq_pipeline are unloaded.\n")
+end
