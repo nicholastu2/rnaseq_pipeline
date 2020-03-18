117ce79c8bfd3d1e97f0054b5cd58bce1b58257c
cmatkhan <chase.mateusiak@gmail.com>
2020-03-18 12:58:43 -0500

    update checks on attributes in genotypecheck

--- tools/utils.py
+++ tools/utils.py
@@ -466,21 +466,30 @@ def genotypeCheck(standard_data):
     :returns: None. However, outputs in subdir of align_counts a series of IGV screenshots and histograms corresponding to the genes in the
     """
 
-    # verify standard_data has correct attributes and the paths to the query_sheet and raw_counts exit
-    if not (hasattr(standard_data,'query_sheet_path') and hasattr(standard_data,'raw_count_path') and
-            hasattr(standard_data, 'log2_cpm') and standard_data.experiment_columns and
-            os.path.exists(standard_data.query_sheet_path) and
-            os.path.exists(standard_data.raw_count_path) and os.path.exists(standard_data.log2_cpm)):
-        sys.exit('query_sheet_path or raw_count data was either entered to StandardDataFormat incorrectly'
-                 'or the paths do not exist. Check both the code constructing standard_data and the paths')
+    # verify standard_data has correct attributes and the paths to the query_sheet and raw_counts exit TODO: update these checks
+    if not (hasattr(standard_data, 'standardized_query_path') and os.path.exists(standard_data.standardized_query_path)):
+        sys.exit('the StandardDataFormat instance does not have attribute query_sheet_path')
+    elif not (hasattr(standard_data, 'raw_count_path') and os.path.exists(standard_data.raw_count_path)):
+        sys.exit('the StandardDataFormat instance does not have attribute raw_count_path')
+    elif not (hasattr(standard_data, 'log2_cpm_path') and os.path.exists(standard_data.log2_cpm_path)):
+        sys.exit('the StandardDataFormat instance does not have attribute log2_cpm_path')
+    elif not (hasattr(standard_data, 'output_dir') and os.path.exists(standard_data.output_dir)):
+        sys.exit('the StandardDataFormat instance does not have attribute output_dir')
+    elif not standard_data.experiment_columns:
+        print('no experiment columns provided. By default, genotype, timepoint, treatment will be used. Do you wish to continue (y/n)?\n')
+        answer = input()
+        if answer == 'n' | answer == 'No' | answer == N:
+            sys.exit('relaunch script with appropriate experiment columns')
+        setattr(standard_data, 'experiment_columns', ['genotype', 'timepoint', 'treatment'])
+
     else:
         # create script command for genotype_check_histogram.R
-        script_cmd = 'genotype_check_histogram.R -q {} -c {} -o {}'.format(standard_data.query_sheet_path,
-                                                                              standard_data.log2_cpm,
-                                                                              standard_data.output_dir)
+        script_cmd = 'genotype_check_histogram.R -q {} -c {} -o {}'.format(standard_data.standardized_query_path,
+                                                                           standard_data.log2_cpm_path,
+                                                                           standard_data.output_dir)
         exp_column_statement = ' -e '
         for i in standard_data.experiment_columns:
-            exp_column_statement = exp_column_statement +'{},'.format(i)
+            exp_column_statement = exp_column_statement + '{},'.format(i)
         exp_column_statement = exp_column_statement[:-1]
         script_cmd = script_cmd + exp_column_statement
         # execute genotype_check_histogram.R
