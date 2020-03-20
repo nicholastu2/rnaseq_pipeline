f475e095988dc984eecb53e3a93a7a4d3d83049f
cmatkhan <chase.mateusiak@gmail.com>
2020-03-20 14:35:48 -0500

    debug OrganismData

--- rnaseq_tools/OrganismData.py
+++ rnaseq_tools/OrganismData.py
@@ -5,19 +5,16 @@ import os
 
 class OrganismData(StandardData):
     def __init__(self, **kwargs):
-        # set (overwrite?) self_type depending on cmdline input
-        self.self_type = 'OrganismData'
-        # and configure
-        utils.configure(self, self.config_file, self.type)
-        self.list_of_known_organisms = ['H99', 'KN99', 'S288C_R64']
+        # set list of known organisms
+        self._configured_organisms_list = ['H99', 'KN99', 'S288C_R64']
         # initialize Standard data with the extended _attributes
         # recall that this will check for and/or create the directory structure found at
         super(OrganismData, self).__init__(**kwargs)
-        # make sure self_type wasn't overwritten by super call above
+        # overwrite super.self_type with object type of child (this object)
         self.self_type = 'OrganismData'
         # set organism data, if it is passed
         if hasattr(self, 'organism'):
-            if self.organism in self.list_of_known_organisms:
+            if self.organism in self._configured_organisms_list:
                 self.setOrganismData()
             else:
                 print('\n{self.organism} is not configured. You will have to set the OrganismData attributes manually. '
@@ -25,6 +22,9 @@ class OrganismData(StandardData):
                       'and create a subdir of genomes_files with an OrganismData_config.ini file, zip it into '
                       '/lts/mblab/Crypto/rnaseq_data/genome_files.zip, remove your genome_files in your {self.user_rnaseq_pipeline} directory'
                       'and either re-run this script or start an interactive python session, import and instantiate a StandardData object.\n')
+        else:
+            setattr(self, 'organism', None)
+            utils.configure(self, self.config_file, self.self_type)
 
     def setOrganismData(self):
         setattr(self, 'organism_config_file', os.path.join(self.user_rnaseq_pipeline, self.genome_files,
