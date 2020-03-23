a59c33e8e37389d9340deaf659ee6260923e7597
cmatkhan <chase.mateusiak@gmail.com>
2020-03-22 23:54:20 -0500

    workign with make_igv_snapshot

--- rnaseq_tools/IgvObject.py
+++ rnaseq_tools/IgvObject.py
@@ -39,13 +39,14 @@ class IgvObject(OrganismData):
     entered by a config file or by the user on instantiation. If they are not set, *typically* they will be set automatically in the script (this is a TODO OBJECT TO TAKE CARE OF THIS BETTER)
     or a print statement will tell you the default location, or an error will be raised (again, TODO)
     """
+
     def __init__(self, **kwargs):
         # additional attributes to add to the _attributes in StandardData
         # TODO: possibly change inheretence to a subclass of OrganismData that sets up a class for ANY scheduler manipulation (ie align_counts, this) that take email as an optional argument
-        self._igv_attributes = ['sample_list', 'igv_genome', 'output_dir', 'wildtype', 'experiment_dir', 'igv_output_dir', 'email']
+        self._igv_attributes = ['sample_list', 'igv_genome', 'output_dir', 'wildtype', 'experiment_dir',
+                                'igv_output_dir', 'email']
         # initialize Standard data with the extended _attributes
         super(IgvObject, self).__init__(self._igv_attributes, **kwargs)
-
 
     def makeIgvSnapshotDict(self):
         """
@@ -66,9 +67,10 @@ class IgvObject(OrganismData):
                                  'It will just make the searches for a given sample somewhat longer (though likely not much).')
         # if an experiment_dir is not passed in constructor of IgvObject, create one in rnaseq_tmp/<timenow>_igv_files and store the path in self.experiment_dir
         if not hasattr(self, 'experiment_dir'):
-            print('creating a directory in rnaseq_tmp to store alignment files so that the scheduler has access to them.'
-                  'This assumes that the count and alignment files have already been processed and moved to '
-                  '/lts/mblab/Crypto/rnaseq_data/align_expr')
+            print(
+                'creating a directory in rnaseq_tmp to store alignment files so that the scheduler has access to them.'
+                'This assumes that the count and alignment files have already been processed and moved to '
+                '/lts/mblab/Crypto/rnaseq_data/align_expr')
             timestr = time.strftime("%Y%m%d_%H%M%S")
             setattr(self, 'experiment_dir', os.path.join(self.rnaseq_tmp, '{}_igv_files'.format(timestr)))
             utils.mkdirp(self.experiment_dir)
@@ -92,7 +94,7 @@ class IgvObject(OrganismData):
                 utils.executeSubProcess(cmd)
             # store full path to bam file in experiment dir
             bamfile_fullpath = os.path.join(self.experiment_dir, bamfile)
-            if not os.path.exists(bamfile_fullpath + '.bai'): # test if indexed bam exists
+            if not os.path.exists(bamfile_fullpath + '.bai'):  # test if indexed bam exists
                 self.indexBam(bamfile_fullpath)
             # split on period if this is a double perturbation. Regardless of whether a '.' is present,
             # genotype will be cast to a list eg ['CNAG_00000'] or ['CNAG_05420', 'CNAG_01438']
@@ -105,13 +107,14 @@ class IgvObject(OrganismData):
                 # add the genotypes to genotype_list (not as a list of list, but as a list of genotypes)
                 genotype_list.extend(genotype)
                 # add to igv_snapshot_dict
-                igv_snapshot_dict.setdefault(sample, {}).setdefault('gene', []).extend(genotype) # TODO: clean this up into a single line, make function to avoid repeated code below w/wildtype
+                igv_snapshot_dict.setdefault(sample, {}).setdefault('gene', []).extend(
+                    genotype)  # TODO: clean this up into a single line, make function to avoid repeated code below w/wildtype
                 igv_snapshot_dict[sample]['bam'] = bamfile_fullpath
                 igv_snapshot_dict[sample]['bed'] = None
             # if genotype is equal to wildtype, then store the sample as the wildtype (only one, check if this is right)
             else:
                 igv_snapshot_dict.setdefault(sample, {'gene': None, 'bam': None, 'bed': None})
-                wildtype_sample_list.append([sample, bamfile_fullpath]) # wildtype_sample_list will be a list of lists
+                wildtype_sample_list.append([sample, bamfile_fullpath])  # wildtype_sample_list will be a list of lists
         # if the wildtype genotype was found, create entry in the following form
         # {sample_read_counts.tsv: {'gene': [perturbed_gene_1, perturbed_gene_2, ...], 'bam': wt.bam, 'bed': created_bed.bed}
         for wt_sample in wildtype_sample_list:
@@ -129,13 +132,13 @@ class IgvObject(OrganismData):
         """
         print('\nindexing {}'.format(bamfile_fullpath))
         # subprocess.call will wait until subprocess is complete
-        exit_status = subprocess.call('samtools index -b {}'.format(bamfile_fullpath), shell = True)
+        exit_status = subprocess.call('samtools index -b {}'.format(bamfile_fullpath), shell=True)
         if exit_status == 0:
             print('\nindexing complete. .bai is deposited in {}'.format(self.experiment_dir))
         else:
             sys.exit('failed to index {}. Cannot continue'.format(bamfile_fullpath))
 
-    def createBedFile(self, flanking_region = 500, file_format='png'):
+    def createBedFile(self, flanking_region=500, file_format='png'):
         """
         Create bed files to describe IGV region of interest
         :param flanking_region: how far up/down stream from the gene to capture in the snapshot
@@ -149,18 +152,18 @@ class IgvObject(OrganismData):
         elif self.annotation_file.endswith('gff') or self.annotation_file.endswith('gff3'):
             self.annotation_dict = annotation_tools.parseGff3(self.annotation_file)
         else:
-            sys.exit("ERROR: The gene annotation format cannot be recognized.") # TODO: clean up preceeding blocks -- move parseGFF to OrganismData
+            sys.exit(
+                "ERROR: The gene annotation format cannot be recognized.")  # TODO: clean up preceeding blocks -- move parseGFF to OrganismData
         ## create gene body region bed file
         for sample in self.igv_snapshot_dict.keys():
             igv_bed_filepath = os.path.join(self.experiment_dir, utils.pathBaseName(sample) + '.bed')
             self.igv_snapshot_dict[sample]['bed'] = igv_bed_filepath
-            writer = open(igv_bed_filepath, 'w')
-            for gene in self.igv_snapshot_dict[sample]['gene']:
-                d = self.annotation_dict[gene]
-                writer.write('%s\t%d\t%d\t[%s]%s.%s\n' % \
-                             (d['chrm'], d['coords'][0] - flanking_region,
-                              d['coords'][1] + flanking_region, sample, gene, file_format))
-
+            with open(igv_bed_filepath, 'w') as file:
+                for gene in self.igv_snapshot_dict[sample]['gene']:
+                    d = self.annotation_dict[gene]
+                    file.write('%s\t%d\t%d\t[%s]%s.%s\n' % (d['chrm'], d['coords'][0] - flanking_region,
+                                                            d['coords'][1] + flanking_region, sample, gene,
+                                                            file_format))
 
     def writeIgvJobScript(self, fig_format='png'):
         """
