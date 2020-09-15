dfc24cd51965f8935d6f9a7e3a3f664f3a5ca3ea
cmatkhan <chase.mateusiak@gmail.com>
2020-09-14 21:12:55 -0500

    igvobject debug

--- tools/make_IGV_snapshots.py
+++ tools/make_IGV_snapshots.py
@@ -283,6 +283,7 @@ def main(input_files, region_file='regions.bed', genome='hg19', image_height='50
     # add count to ensure file is not overwritten
     while os.path.exists(batchscript_file):
         batchscript_file = os.path.join(outdir, 'IGV_snapshots_%s.bat' %str(count))
+        count = count + 1
 
     # make sure the regions file exists
     file_exists(region_file, kill=True)
