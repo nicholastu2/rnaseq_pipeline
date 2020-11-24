c77ca63ab69b3696dfcbfa1181e2772f165e4293
cmatkhan <chase.mateusiak@gmail.com>
2020-11-23 20:32:54 -0600

    adding library protocol specific size factor functionality

--- tools/deseq_de.R
+++ tools/deseq_de.R
@@ -13,6 +13,7 @@ main = function(parsed_cmd_line_args){
   raw_counts_df_path = parsed_cmd_line_args$raw_counts
   metadata_df_path = parsed_cmd_line_args$metadata
   size_factors = parsed_cmd_line_args$size_factors
+  calculate_size_factors = parsed_cmd_line_args$calculate_size_factors
   factor_column_list = strsplit(parsed_cmd_line_args$factor_column_list, ",")
   design_matrix_path = parsed_cmd_line_args$design_matrix
   intercept_only_flag = parsed_cmd_line_args$intercept_only_flag
@@ -39,9 +40,27 @@ main = function(parsed_cmd_line_args){
   print('...reading in design matrix')
   model_matrix = as.matrix(read_csv(design_matrix_path))
   
+  # this (see below) is part of the flag to calculate the protocol specific size factors in the script 20201123
+  if(calculate_size_factors){
+    protocol_size_factors = calculateProtocolSizeFactors(metadata_df, raw_counts_df)
+    df = as.data.frame(protocol_size_factors)
+    rownames(df) = names(protocol_size_factors)
+    write.csv(df, paste(output_path, 'protocol_specific_size_factors.csv', sep='/'))
+    raw_counts_df = orderCountColumns(raw_counts_df, protocol_size_factors)
+    write_csv(raw_counts_df, paste(output_path, 'raw_counts_ordered_by_size_factors.csv', sep='/'))
+    metadata_df = orderMetadataRows(metadata_df, protocol_size_factors)
+    write_csv(raw_counts_df, paste(output_path, 'metadata_ordered_by_size_factors.csv', sep='/'))
+  }
+  
   print('...construct deseq model')
   dds = createDeseqDataObject(raw_counts_df, metadata_df, model_matrix)
   
+  # this (see above) is part of the flag to calculate the protocol specific size factors in the script 20201123
+  if(calculate_size_factors){
+    sizeFactors(dds) = protocol_size_factors
+  }
+  
+  # NOTE: THIS IS FOR USER SUBMISSION AND IS A DEFINITE TODO IN TERMS OF ERROR HANDLING. THIS SHOULD BE USED IF THE USER IS SUBMITTING SIZE FACTORS, NOT IF THEY WANT TO CALCULATE PROTOCOL SIZE FACTORS IN THE SCRIPT
   if(!is.null(size_factors)){
     size_factors_list = read.csv(size_factors)[,1]
     sizeFactors(dds) = size_factors_list
@@ -88,6 +107,53 @@ libraryProtocolDateTest = function(column_list){
   
 } # end libraryProtocolDateTest()
 
+calculateProtocolSizeFactors = function(metadata_df, raw_counts){
+  #' new function 20201123 to calculate protocol specific size factors -- needs editing/debugging/error handling in script
+  #' scripting here is copied from notebook -- needs cleaning up
+  
+  old_library_metadata_df = metadata_df %>% filter(LIBRARYPROTOCOL == "SolexaPrep")
+  new_library_metadata_df = metadata_df %>% filter(LIBRARYPROTOCOL != "SolexaPrep")
+
+  old_library_counts = as_tibble(raw_counts) %>% select(old_library_metadata_df$FASTQFILENAME)
+  new_library_counts = as_tibble(raw_counts) %>% select(new_library_metadata_df$FASTQFILENAME)
+  
+  old_library_counts_na_to_0 = replace(old_library_counts, is.na(old_library_counts), 0)
+  new_library_counts_na_to_0 = replace(new_library_counts, is.na(new_library_counts), 0)
+  
+  dds_old = DESeqDataSetFromMatrix(colData=old_library_metadata_df, countData=old_library_counts_na_to_0, design=~1)
+  dds_old = estimateSizeFactors(dds_old)
+  old_size_factors = sizeFactors(dds_old)
+  
+  dds_new = DESeqDataSetFromMatrix(colData=new_library_metadata_df, countData=new_library_counts_na_to_0, design=~1)
+  dds_new = estimateSizeFactors(dds_new)
+  new_size_factors = sizeFactors(dds_new)
+  
+  size_factors_by_protocol = c(old_size_factors, new_size_factors)
+  
+  return(size_factors_by_protocol)
+  
+} # end calculateProtocolSizeFactors()
+
+orderCountColumns = function(raw_counts, size_factors_by_protocol){
+  #' order columns of counts based on order of size factors
+  counts_ordered = raw_counts[names(size_factors_by_protocol)]
+  counts_ordered = replace(counts_ordered, is.na(counts_ordered), 0)
+  
+  return(counts_ordered)
+    
+} # end orderCountColumns()
+
+orderMetadataRows = function(metadata_df, size_factors_by_protocol){
+  #' order rows of metadata based on order of size factors
+  
+  metadata_df_ordered = metadata_df[match(names(size_factors_by_protocol), metadata_df$FASTQFILENAME),]
+  
+  return(metadata_df_ordered)
+  
+} # end orderMetadataRows()
+
+
+
 createDeseqDataObject = function(raw_count_df, metadata_df, model_matrix){
   
   # generate deseq dataset (summarized experiment object) from count, metadata and design formula
@@ -129,6 +195,8 @@ parseArguments <- function() {
     make_option(c('-s', '--size_factors'), 
                 help='Use this argument if size factors are not calculated across all samples. 
                       If unused, size factors calculated automatically by DESeq'),
+    make_option(c('-c', '--calculate_size_factors'), action='store_true', default=FALSE,
+                help='Set this flag to calculate, within the script, and use size factors by library protocol'),
     make_option(c('-f', '--factor_column_list'), 
                 help='comma separated list NO SPACES of columns to factor, eg LIBRARYDATE, GENOTYPE'),
     make_option(c('-g', '--genotype_results_flag'), action='store_true',
