92a7b78ff9b94c88738f074d6e2d5bf624cfa1d4
cmatkhan <chase.mateusiak@gmail.com>
2020-12-07 10:11:27 -0600

    reversing order of merge

--- rnaseq_tools/DatabaseObject.py
+++ rnaseq_tools/DatabaseObject.py
@@ -41,8 +41,8 @@ class DatabaseObject(StandardData):
         try:
             self.database_subdirectories = kwargs['database_subdirectories']
         except KeyError:
-            # self.database_subdirectories = ['fastqFiles', 'library', 's2cDNASample', 's1cDNASample', 'rnaSample', 'bioSample'] # concat in reverse order
-            self.database_subdirectories = ['bioSample', 'rnaSample', 's1cDNASample', 's2cDNASample', 'library', 'fastqFiles']
+            self.database_subdirectories = ['fastqFiles', 'library', 's2cDNASample', 's1cDNASample', 'rnaSample', 'bioSample'] # concat in reverse order
+            #self.database_subdirectories = ['bioSample', 'rnaSample', 's1cDNASample', 's2cDNASample', 'library', 'fastqFiles']
 
         # see setter setFilterJson()
         self.filter_json = None
